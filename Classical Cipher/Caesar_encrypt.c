88814a3fc7f36b8174dcdda7869bbba53b34f087
taeyoon02 <parktaeyun02@korea.ac.kr>
2022-02-20 01:32:30 +0900

    카이사르 50% 완성
    시작이 반이다 (실제로 반 했음)

--- Classical Cipher/Caesar_encrypt.c
+++ Classical Cipher/Caesar_encrypt.c
@@ -1,3 +1,19 @@
-// 있어야 하는 것 : 입력, 출력, AES 기반의 알파벳 이동
+#include <stdio.h>
 
-// AES 함수에서 주목할 만한거 
+void encrypt();
+
+void decrypt();
+
+void Caesar_attack();
+
+int main() {
+	char plaintext[] = "hello";
+	for (int i = 0; plaintext[i] != '\0'; i++) {
+		plaintext[i] = ((plaintext[i] - 96 + 3) % 27) + 96;
+	}
+
+	for (int i = 0; plaintext[i] != '\0'; i++) {
+		printf("%c", plaintext[i]);
+	}
+} // 일단 암호화만 해둠 근데 이게 맞나 싶기도 하고..
+
